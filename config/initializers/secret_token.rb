8af15c21fb792d5c4e98d24e940b2b4835a71f39
telu <jnunez@inetsec.es>
2013-07-03 00:18:30 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instituto::Application.config.secret_token = 'fe0ea71fe8d3378b4f1b86836c686854251150f2d66c0d2b80ee3734be6d7f0c97ecebb3f0dcc7ccd9de7d0b9cd603a42ad4386ffea5e40db48a99b71dbf3920'
